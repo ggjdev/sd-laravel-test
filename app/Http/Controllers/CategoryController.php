b7c00ceba0173ea923724a8acd785d2ce8d5a242
Geoff <168197965+ggjdev@users.noreply.github.com>
2025-01-22 22:35:04 +0000

    Add index method return type

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         $categories = $request->user()->categories;
 
